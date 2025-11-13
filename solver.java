698e0f531945eb4bb7601315830d326bd12988b1
Toon Paul <hudallap@gmail.com>
2025-11-13 17:20:55 +0000

    ideas

--- solver.java
+++ solver.java
@@ -1,1 +1,32 @@
+//Name: Paul Hudalla
+//Date: 11/10/25
+//this program will check whether a string is valid or not
 
+public class solver{
+    public static void isValid(String s){
+        String firstCharcter = s.substring(0,1);
+        s = s.substring(1);
+        if(firstCharacter.equals("(")){
+            for(int i = 0; i < s.length(); i++){
+                if(s.chatAt(i).equals(")")){
+                    s = s.substring(0,i) + s.substring(i+1);
+                }
+            }
+        } else if (firstCharacter.equals("[")){
+            for(int i = 0; i < s.length(); i++){
+                if(s.chatAt(i).equals("]")){
+                    s = s.substring(0,i) + s.substring(i+1);
+                }
+        } else if (firstCharacter.equals("{")){
+            for(int i = 0; i < s.length(); i++){
+                if(s.chatAt(i).equals("}")){
+                    s = s.substring(0,i) + s.substring(i+1);
+                }
+        } else{
+            return false;
+        }
+    }
+    public static void main(String[] args){
+
+    }
+}
