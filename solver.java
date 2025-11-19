7250bb0afb8b5c95871a5e4a91436f0ea1794f71
Toon Paul <hudallap@gmail.com>
2025-11-19 19:18:01 +0000

    done:)

--- solver.java
+++ solver.java
@@ -4,7 +4,7 @@
 
 public class solver {
     public static boolean isValid(String s) {
-        s = s.substring(1);
+        //s = s.substring(1);
         while (s.length() > 0) {
             boolean removed = false;
             for (int i = 0; i < s.length() - 1; i++) {
@@ -15,22 +15,23 @@ public class solver {
                     }
                 }
                     // check for the other symbols
-                if (s.substring(i, i + 1).equals("[")) {
+                else if (s.substring(i, i + 1).equals("[")) {
                     if (s.substring(i + 1, i + 2).equals("]")) {
                         s = s.substring(0, i) + s.substring(i + 2);
                         removed = true;
                     }
                 }
-                if (s.substring(i, i + 1).equals("{")) {
+                else if (s.substring(i, i + 1).equals("{")) {
                     if (s.substring(i + 1, i + 2).equals("}")) {
                         s = s.substring(0, i) + s.substring(i + 2);
                         removed = true;
                     }
-                if (removed == false) {
-                        return false;
-                    }
+               
                 }
             }
+             if (removed == false) {
+                        return false;
+                    }
 
         }
         return true;
@@ -38,12 +39,12 @@ public class solver {
 
     public static void main(String[] args) {
         System.out.println(isValid("(){}[]")); //true
-        // System.out.println(isValid("(]")); //false
-        // System.out.println(isValid("([{({})}])")); //true
-        // System.out.println(isValid("(([]){})")); //true
-        // System.out.println(isValid("{}[())()(]")); //false
-        // System.out.println(isValid("(){}{")); //false
-        // System.out.println(isValid("([)]")); //false
-        // System.out.println(isValid("([{}([)])]")); //false
+        System.out.println(isValid("(]")); //false
+        System.out.println(isValid("([{({})}])")); //true
+        System.out.println(isValid("(([]){})")); //true
+        System.out.println(isValid("{}[())()(]")); //false
+        System.out.println(isValid("(){}{")); //false
+        System.out.println(isValid("([)]")); //false
+        System.out.println(isValid("([{}([)])]")); //false
     }
 }
